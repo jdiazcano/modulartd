fc1e860b0ec077a9d8ff65650fc7bdd156dbe8f9
Javier Diaz-Cano <terkitos@gmail.com>
2016-11-16 23:33:23 +0100

    Defined the Tile class

--- editor/core/src/main/kotlin/com/jdiazcano/modulartd/beans/Tile.kt
+++ editor/core/src/main/kotlin/com/jdiazcano/modulartd/beans/Tile.kt
@@ -1,7 +1,15 @@
 package com.jdiazcano.modulartd.beans
 
 /**
+ * Tile class that defines the piece of the terrain that will be constructing the grid.
+ *
  * Created by javierdiaz on 16/11/2016.
  */
-class Tile {
+class Tile(
+        name: String,
+        resource: Resource,
+        rotationAngle: Float,
+        var buildable: Boolean
+) : MapObject(name, resource, rotationAngle), Scriptable {
+    override var script: String = ""
 }
